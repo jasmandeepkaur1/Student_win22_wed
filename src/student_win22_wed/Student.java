e6890191e3a2ae5b5907ec90f35bdeb369e534c6
kau11071 <kau11071@sheridancollege.ca>
2022-02-02 14:00:11 -0500

    added new new variable address

--- src/student_win22_wed/Student.java
+++ src/student_win22_wed/Student.java
@@ -12,7 +12,15 @@ package student_win22_wed;
 public class Student {
     private String studentId;
     private String studentName;
-    
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
     public Student(String studentId,String studentName){
         this.studentId = studentId;
         this.studentName=studentName;                
